114eabb171413cdcaf02a8338d27bcd47ecd14c9
Siobhan Emma Forsyth <s.e.forsyth@hotmail.co.uk>
2024-02-27 18:40:10 +0000

    Include database in publish

--- KudosDash/Data/ApplicationDbContext.cs
+++ KudosDash/Data/ApplicationDbContext.cs
@@ -4,19 +4,19 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KudosDash.Data
-{
-	public class ApplicationDbContext : IdentityDbContext<AppUser>
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
+	public class ApplicationDbContext : IdentityDbContext<AppUser>
 		{
-		}
+		public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+			{
+			}
 
 		public DbSet<AppUser> Account { get; set; }
 
 		public DbSet<Feedback> Feedback { get; set; }
 
 		public DbSet<Teams> Teams { get; set; }
-		public DbSet<KudosDash.Models.Users.AccountVM> AccountVM { get; set; } = default!;
+		public DbSet<AccountVM> AccountVM { get; set; } = default!;
+		}
 	}
-}
