23468e693f540b2715ae02fcac38267433eb8763
Siobhan Emma Forsyth <s.e.forsyth@hotmail.co.uk>
2024-03-05 18:12:24 +0000

    Update cypress tests to run in pipeline

---
+++ KudosDash.Tests/Cypress/security/SQLInjection.Spec.cy.js
@@ -0,0 +1,9 @@
+﻿describe("SQL injection testing", function () {
+    it("Checks for SQL injection vulnerability on Login form", function () {
+        cy.visit("http://localhost:5289/Account/Login?query=1 OR 1=1");
+        cy.contains("Error").should("not.exist");
+    })
+    it("Checks for SQL injection vulnerability on Admin page", function () {
+
+    })
+})
