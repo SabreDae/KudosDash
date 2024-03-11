a3e74a294cf645b117f4bcea4864176e5764e883
Siobhan Forsyth <forsysio@amazon.com>
2024-03-11 18:32:04 +0000

    Add e2e test for session handling

---
+++ KudosDash.Tests/Cypress/e2e/Timeout.Spec.cy.js
@@ -0,0 +1,23 @@
+describe("Test session timeout", function () {
+    it("Should be auto-logged out after 3 minutes", function () {
+        cy.clock()
+        cy.visit("http://localhost:5289/Account/Register")
+        cy.get("#FirstName").type("Test")
+        cy.get("#LastName").type("Test")
+        cy.get("#Role").select(2)
+        cy.get("#Email").type("test@test.com")
+        cy.get("#Password").type("Test-1234")
+        cy.get("#ConfirmPassword").type("Test-1234")
+        cy.get("input[value='Register']").click()
+        // Session should time out after 3 minutes aka 180000 milliseconds
+        cy.wait(180000)
+        cy.contains("Submit Feedback").click()
+        cy.url().should("include", "/Account/Login")
+        // Cleanup by deleting test account
+        cy.get("#Email").type("test@test.com")
+        cy.get("#Password").type("Test-1234")
+        cy.get("input[value='Login']").click()
+        cy.visit("http://localhost:5289/Account/Delete")
+        cy.get("button[value='Delete'").click()
+    })
+})
