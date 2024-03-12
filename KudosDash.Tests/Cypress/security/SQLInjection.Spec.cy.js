5619a8cb59f76e63d429e293bb2684027ed7dc28
Siobhan Emma Forsyth <s.e.forsyth@hotmail.co.uk>
2024-03-12 20:17:06 +0000

    Implement remaining Feedback e2e tests

--- KudosDash.Tests/Cypress/security/SQLInjection.Spec.cy.js
+++ KudosDash.Tests/Cypress/security/SQLInjection.Spec.cy.js
@@ -13,8 +13,4 @@
         cy.visit("http://localhost:5289/Admin?query=1 OR 1=1");
         cy.contains("Error").should("not.exist");
     })
-    it("Checks for SQL injection vulnerability on Registration page", function () {
-        cy.visit("http://localhost:5289/Account/Register?query=1 OR 1=1");
-        cy.contains("Error").should("not.exist");
-    })
 })
